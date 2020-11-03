4e50a38efea664cad508db5bfc72e68dc7061b73
Cameron <cameron.wills89@outlook.com>
2020-11-03 09:04:31 -0500

    initial files

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+// import the Sequelize constructor from the library
+// const Sequelize = require("sequelize");
+
+//import my credentials from .env
+// require("dotenv").config();
+
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//       host: "localhost",
+//       dialect: "mysql",
+//       dialectOptions: {
+//         decimalNumbers: true,
+//       },
+//     });
+
+// module.exports = sequelize;
---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS auto_tracker_db;
+
+CREATE DATABASE auto_tracker_db;
