38f3b4d52a5212296d51f49be7154fcfe22f9d52
Cameron <cameron.wills89@outlook.com>
2020-11-04 09:32:43 -0500

    added in email and password

--- models/Owner.js
+++ models/Owner.js
@@ -19,6 +19,21 @@ Owner.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4],
+      },
+    },
   },
   {
     sequelize,
