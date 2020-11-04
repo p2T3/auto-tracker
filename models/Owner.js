e780c626d4273dd9cafa0224ce1126250569b873
Cameron <cameron.wills89@outlook.com>
2020-11-03 22:38:34 -0500

    Models created

---
+++ models/Owner.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Owner extends Model {}
+
+Owner.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "owner",
+  }
+);
+
+module.exports = Owner;
