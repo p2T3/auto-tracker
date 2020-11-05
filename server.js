b7d15264b53a4f486a6c58a5c161e378cc41aa82
Owais Islam <owaisislam@gmail.com>
2020-11-04 18:54:10 -0600

    set up server.js

--- server.js
+++ server.js
@@ -1,2 +1,43 @@
-//main server file
-//something to commit
+const path = require('path');
+const express = require('express');
+// const routes = require('./controllers');
+const sequelize = require('./config/connection');
+// const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
+// const hbs = exphbs.create({
+//     helpers
+// });
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: process.env.DB_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+        checkExpirationInterval: 1000 * 60 * 10, // will check every 10 minutes
+        expiration: 1000 * 60 * 30 // will expire after 30 minutes
+    })
+};
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(session(sess));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+// app.use(routes);
+
+sequelize.sync();
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+});
