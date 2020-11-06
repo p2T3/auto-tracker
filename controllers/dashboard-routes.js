d40ebb4c8324fa348bba6da917bcd66852e81f99
RichA <rich_ay@sbcglobal.net>
2020-11-06 17:12:52 -0600

    Rendered driver page with data.

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -25,11 +25,48 @@ router.get("/vehicle", (req, res) => {
     ],
   })
   .then(dbAutoData => {
-    console.log( "dbAutoData:", dbAutoData );
+    //console.log( "dbAutoData:", dbAutoData );
     // serialize data before passing to template
     const autos = dbAutoData.map(auto => auto.get({ plain: true }));
-    console.log( "Autos:", autos );
+    //console.log( "Autos:", autos );
     res.render('vehicle-dashboard', autos);
+    //res.render('vehicle-dashboard', { autos, loggedIn: true });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+//////////////////////////////////////////////////////////////////////////////////////
+router.get("/driver", (req, res) => {
+
+  Auto.findAll({
+
+    // where: {
+      
+    //  id: req.params.id
+    // },
+    attributes: {
+      exclude: ["createdAt", "updatedAt"],
+    },
+    include: [
+      // {
+      //   model: Owner,
+      //   attributes: ["id", "first_name", "last_name", "email"],
+      // },
+      {
+        model: Driver,
+        attributes: ["id", "first_name", "last_name", "relation"],
+      },
+    ],
+  })
+  .then(dbAutoData => {
+    //console.log( "dbAutoData:", dbAutoData );
+    // serialize data before passing to template
+    const autos = dbAutoData.map(auto => auto.get({ plain: true }));
+    //console.log( "Autos:", autos );
+    res.render('driver-dashboard', autos);
     //res.render('vehicle-dashboard', { autos, loggedIn: true });
   })
   .catch(err => {
