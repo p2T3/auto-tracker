bda5894932b8db2b421e6aa61ff61ee2d2167132
Cameron <cameron.wills89@outlook.com>
2020-11-05 21:36:43 -0500

    added handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,5 +2,8 @@ const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { Owner, Auto, Driver } = require("../models");
 
+router.get("/", (req, res) => {
+  res.render("homepage");
+});
 
 module.exports = router;
