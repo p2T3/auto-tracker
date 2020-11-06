9d918fa11fa57b96eb82aebb9d296c6ff194913e
Cameron <cameron.wills89@outlook.com>
2020-11-05 21:55:10 -0500

    added handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,4 +6,11 @@ router.get("/", (req, res) => {
   res.render("homepage");
 });
 
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
 module.exports = router;
