08e865167ed5fd1194f992fc5449c6ffaf1b6d37
Owais Islam <owaisislam@gmail.com>
2020-11-09 18:12:14 -0600

    added functionality for homepage Get Started button

---
+++ public/javascript/get-started.js
@@ -0,0 +1,10 @@
+async function getStarted(event) {
+    event.preventDefault();
+
+    document.location.replace("/signup");
+}
+
+
+document
+    .querySelector("#get-started")
+    .addEventListener("click", getStarted);
