bcf449dda50284f9d2ce45258466c5f9f243ae98
Owais Islam <owaisislam@gmail.com>
2020-11-12 11:50:28 -0600

    attempted login fix

---
+++ public/javascript/modify-vehicle.js
@@ -0,0 +1,9 @@
+async function deleteVehicle(event) {
+    event.preventDefault();
+
+    console.log(this.id);
+}
+
+document
+    .querySelector("#delete-vehicle")
+    .addEventListener("click", deleteVehicle);
