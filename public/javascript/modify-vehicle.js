755112647d31f2ae88fa3d9ce4aae318a5f990f8
Cameron <cameron.wills89@outlook.com>
2020-11-13 22:12:17 -0500

    testing getting the vehicle ids

--- public/javascript/modify-vehicle.js
+++ public/javascript/modify-vehicle.js
@@ -1,19 +1,28 @@
-async function deleteVehicle(event) {
-  event.preventDefault();
-}
+// async function deleteVehicle(event) {
+//   event.preventDefault();
+// }
 
-//   $(".modal").text($(this).attr("data-id"));
-document
-  .querySelector("#delete-vehicle")
-  .addEventListener("click", deleteVehicle);
+// //   $(".modal").text($(this).attr("data-id"));
+// document
+//   .querySelector("#delete-vehicle")
+//   .addEventListener("click", deleteVehicle);
+
+document.querySelector("#vehicle-card").addEventListener("click", function (event) {
+  let vehicleId = document.querySelector(".modal-car-id").innerHTML;
+
+  console.log("PLEASE GIVE ME ID", vehicleId);
+});
+
+//   let asdghdsag = document.querySelector(".card").value;
+//   console.log("PLEASE GIVE ME ID",asdghdsag);
 
 // document
 //   .querySelector("#save-vehicle")
 //   .addEventListener("click", function (event) {
-//     let dataId = event.target.getAttribute("data-id");
+//     // let dataId = event.target.getAttribute("data-id");
 
-//     console.log(dataId);
+//     let vehicleId = document.querySelector(".modal-car-id").innerHTML;
+
+//     console.log("PLEASE GIVE ME ID",vehicleId);
+   
 //   });
-
-
-  
